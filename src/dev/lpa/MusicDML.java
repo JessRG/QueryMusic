fff8bb9ab4b386822dbd467ba7bb8ce928c11665
Jesse <jesusgarza2511@gmail.com>
2025-04-23 22:16:38 -0500

    Add insertRecord method to handle inserting new records into the database

--- src/dev/lpa/MusicDML.java
+++ src/dev/lpa/MusicDML.java
@@ -14,9 +14,11 @@ public class MusicDML {
         ) {
             String tableName = "music.artists";
             String columnName = "artist_name";
-            String columnValue = "Elf";
+            String columnValue = "Bob Dylan";
             if (!executeSelect(statement, tableName, columnName, columnValue)) {
                 System.out.println("Maybe we should add this record");
+                insertRecord(statement, tableName, new String[] {columnName},
+                        new String[] {columnValue});
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -47,7 +49,7 @@ public class MusicDML {
 
     private static boolean executeSelect(Statement statement, String table,
                                          String columnName, String columnValue)
-        throws SQLException {
+            throws SQLException {
 
         String query = "SELECT * FROM %s WHERE %s='%s'"
                 .formatted(table, columnName, columnValue);
@@ -57,5 +59,22 @@ public class MusicDML {
             return printRecords(rs);
         }
         return false;
+    }
+
+    private static boolean insertRecord(Statement statement, String table,
+                                        String[] columnNames, String[] columnValues)
+            throws SQLException {
+
+        String colNames = String.join(",", columnNames);
+        String colValues = String.join("','", columnValues);
+        String query = "INSERT INTO %s (%s) VALUES ('%s')"
+                .formatted(table, colNames, colValues);
+        System.out.println(query);
+        boolean insertResult = statement.execute(query);
+        int recordsInserted = statement.getUpdateCount();
+        if (recordsInserted > 0) {
+            executeSelect(statement, table, columnNames[0], columnValues[0]);
+        }
+        return recordsInserted > 0;
     }
 }
